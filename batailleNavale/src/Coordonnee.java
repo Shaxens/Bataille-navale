434cec9d20c25e5f52ce4e152e5e6e59ddfa184e
DrYruo <oury.jordan@hotmail.com>
2022-04-21 19:27:44 +0200

    maj des classes

--- batailleNavale/src/Coordonnee.java
+++ batailleNavale/src/Coordonnee.java
@@ -1,2 +1,29 @@
-public class Coordonnee {
+public class Coordonnee
+{
+    // ATTRIBUTS
+    private char positionX;
+    private int positionY;
+
+    // CONSTRUCTEUR
+    public Coordonnee(char positionX, int postionY)
+    {
+        this.setPositionX(positionX);
+        this.setPositionY(postionY);
+    }
+
+    // GETTERS
+    public int getPositionX() {
+        return positionX;
+    }
+    public int getPositionY() {
+        return positionY;
+    }
+
+    // SETTERS
+    public void setPositionX(char positionX) {
+        this.positionX = positionX;
+    }
+    public void setPositionY(int positionY) {
+        this.positionY = positionY;
+    }
 }
--- batailleNavale/src/Navire.java
+++ batailleNavale/src/Navire.java
@@ -7,5 +7,53 @@ public class Navire
     private TypeNavire type;
     private int longueur;
     private ArrayList<Coordonnee> position;
+    private boolean inFlotte;
 
+    // Constructeur
+    public Navire(int id, TypeNavire type, int longueur)
+    {
+        this.setId(id);
+        this.setType(type);
+        this.setLongueur(longueur);
+        this.position = new ArrayList<>();
+        this.inFlotte = false;
+    }
+
+    // GETTERS
+    public int getId() {
+        return id;
+    }
+    public TypeNavire getType() {
+        return type;
+    }
+    public int getLongueur() {
+        return longueur;
+    }
+    public ArrayList<Coordonnee> getPosition() {
+        return position;
+    }
+    public boolean isInFlotte() {
+        return inFlotte;
+    }
+
+    // SETTERS
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setType(TypeNavire type) {
+        this.type = type;
+    }
+
+    public void setLongueur(int longueur) {
+        this.longueur = longueur;
+    }
+
+    public void setPosition(ArrayList<Coordonnee> position) {
+        this.position = position;
+    }
+
+    public void setInFlotte(boolean inFlotte) {
+        this.inFlotte = inFlotte;
+    }
 }
