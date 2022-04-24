3960ba9e680c2a88afc8399138f0d84c567b7bf4
DrYruo <oury.jordan@hotmail.com>
2022-04-24 13:39:14 +0200

    Ajout de la librairie mysql-connector-java au projet

--- batailleNavale/src/sql/MySqlNavire.java
+++ batailleNavale/src/sql/MySqlNavire.java
@@ -34,35 +34,11 @@ public class MySqlNavire implements DAO<Navire>
         {
             Connection laConnexion = creeConnexion();
             PreparedStatement requete = laConnexion.prepareStatement("SELECT * FROM NAVIRE");
-            requete.setInt(1,id); // parameterIndex = 1 car on veut 'id' à la place du 1er '?'
             ResultSet res = requete.executeQuery();
-            if (res.next())
-            {
-                int idEtu = res.getInt("id_etudiant");
-                String ine = res.getString("ine_etudiant");
-                String nom = res.getString("nom_etudiant");
-                String prenom = res.getString("prenom_etudiant");
-                String diplome = res.getString("diplome_etudiant");
-                int idPromo = res.getInt("id_promotion");
-
-                System.out.println(idEtu + " " + ine + " " + nom + " " + prenom + " " + diplome + " " + idPromo);
-                instanceEtudiant = new Etudiant(ine,nom,prenom,idPromo);
-                /*instanceEtudiant.setIne(ine);
-                instanceEtudiant.setNom(nom);
-                instanceEtudiant.setPrenom(prenom);
-                instanceEtudiant.setIdPromo(idPromo);*/
-            }
-            if (res != null)
-                res.close();
-            if (requete != null)
-                requete.close();
-            if (laConnexion != null)
-                laConnexion.close();
-        } catch (SQLException sqle) {
-            System.out.println("Pb select " + sqle.getMessage());
         }
-        return instanceEtudiant;
-
+        catch (SQLException e)
+        {
+            e.printStackTrace();
+        }
         return null;
     }
-}
