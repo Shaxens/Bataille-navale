e23f3e9656f54a5524cabf9f152d36312b48fda3
Shaxens <arthurvietti@gmail.com>
2022-04-25 17:15:23 +0200

    Initialisation classes Score et Joueur

--- batailleNavale/src/fr/navflex/Main.java
+++ batailleNavale/src/fr/navflex/Main.java
@@ -1,6 +1,8 @@
 package fr.navflex;
 
 import fr.navflex.gameplay.navire.Flotte;
+import fr.navflex.gameplay.navire.Navire;
+import fr.navflex.joueur.Score;
 
 public class Main {
     public static void main(String[] args)
