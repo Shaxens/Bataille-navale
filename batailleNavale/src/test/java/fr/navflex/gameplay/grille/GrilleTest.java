3a8ebc8bf28c94f076d20aabb1a71eeb677028a9
Farronilucas <farronilucas@hotmail.fr>
2022-05-20 09:32:43 +0200

    testConnexion

--- batailleNavale/src/test/java/fr/navflex/gameplay/grille/GrilleTest.java
+++ batailleNavale/src/test/java/fr/navflex/gameplay/grille/GrilleTest.java
@@ -30,5 +30,9 @@ public class GrilleTest {
 		grille.setGrille(3, 8);
 		Assert.assertNotNull(grille.getGrille());
 	}
-
+	@Test
+	public void testsetGrilleIsEmpty() throws Exception {
+		grille.setGrille(3, 8);
+		Assert.assertIsnotEmpty
+	}
 }
