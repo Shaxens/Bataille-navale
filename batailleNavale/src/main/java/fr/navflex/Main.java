70561e6964f8bdfcc43c7ebac1771ec4bc1535f5
DrYruo <oury.jordan@hotmail.com>
2022-05-26 14:59:34 +0200

    Pause

--- batailleNavale/src/main/java/fr/navflex/Main.java
+++ batailleNavale/src/main/java/fr/navflex/Main.java
@@ -1,6 +1,7 @@
 package fr.navflex;
 
 import fr.navflex.controleur.JoueurControleur;
+import fr.navflex.controleur.PartieControleur;
 import fr.navflex.modele.grille.Coordonnee;
 import fr.navflex.modele.grille.Grille;
 import fr.navflex.modele.joueur.Joueur;
@@ -14,18 +15,8 @@ public class Main
 {
     public static void main(String[] args) throws Exception
     {
-        Moniteur moniteur = new Moniteur();
-        JoueurControleur joueurControleur = new JoueurControleur();
-        RandomizerTools randomizer = new RandomizerTools();
-        Joueur joueur1 = new Joueur(1, "LourdJesus", 6,6);
-        Joueur joueur2 = new Joueur(2, "InfameJuda", 6, 6);
-        joueurControleur.addJoueur(joueur1);
-        joueurControleur.addJoueur(joueur2);
-
-        Grille grille = joueur1.getGrille();
-
-        randomizer.placementDesNaviresRandom(joueur1);
-        randomizer.placementDesNaviresRandom(joueur2);
+        PartieControleur partieControleur = new PartieControleur();
+        partieControleur.initialiserPartie();
 
     }
 }
--- batailleNavale/src/main/java/fr/navflex/controleur/PartieControleur.java
+++ batailleNavale/src/main/java/fr/navflex/controleur/PartieControleur.java
@@ -1,4 +1,91 @@
 package fr.navflex.controleur;
 
+import fr.navflex.modele.joueur.Joueur;
+import fr.navflex.modele.partie.Partie;
+import fr.navflex.modele.partie.Tour;
+import fr.navflex.vue.Moniteur;
+
+import java.util.ArrayList;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class PartieControleur {
+    // ATTRIBUTS
+    Moniteur moniteur = new Moniteur();
+    JoueurControleur controleur = new JoueurControleur();
+
+    // OUTILS INPUT
+    public int inputEntier()
+    {
+        Scanner scanner = new Scanner(System.in);
+
+        while (true)
+        {
+            try {
+                return scanner.nextInt();
+            } catch (InputMismatchException ime) {
+                String mauvaisChoix = scanner.next();
+                this.moniteur.creerMessage("Erreur : Saisie invalide, veuillez saisir un entier : ");
+                continue;
+            }
+        }
+    }
+
+    public int inputEntierPositif()
+    {
+        Scanner scanner = new Scanner(System.in);
+
+        while (true)
+        {
+            try {
+                int input = scanner.nextInt();
+                if (input >= 0) {
+                    return input;
+                }
+                else throw new IllegalArgumentException("Erreur : Saisie invalide, veuillez saisir un entier superieur a 0 : ");
+            } catch (Exception e) {
+                String mauvaisChoix = scanner.next();
+                this.moniteur.creerMessage("Erreur : Saisie invalide, veuillez saisir un entier : ");
+                continue;
+            }
+        }
+    }
+
+    public int inputChoixListeObjetSansId(ArrayList liste)
+    {
+        while (true)
+        {
+            try {
+                int choixDansListe = inputEntierPositif();
+                if (liste.size() < choixDansListe || choixDansListe < 1)
+                {
+                    throw new IllegalArgumentException("Erreur : " + choixDansListe + " n est pas une option de la liste.");
+                }
+                return choixDansListe;
+            } catch (InputMismatchException | IllegalArgumentException ime) {
+                this.moniteur.creerMessage(ime.getMessage());
+                continue;
+            }
+        }
+    }
+
+    // METHODES
+    public Partie initialiserPartie()
+    {
+        moniteur.creerMessage("\nNouvelle partie !\nVeuillez selectionner le nombre de Joueur : ");
+        int nombreJoueur = inputEntierPositif();
+        while (nombreJoueur < 2)
+        {
+            if (nombreJoueur == 0) {
+                moniteur.creerMessage("Si il n'y a aucun joueur on va s'ennuyer, faites un choix plus raisonnable :  ");
+            }
+            else if (nombreJoueur == 1) {
+                moniteur.creerMessage("Ah bonne idee c'est valide ! un mode contre moi-meme arrive bientot !\nMais pour l'instant jouez plutot avec au moins un ami, combien d'ami du coup ? :");
+            }
+            nombreJoueur = inputEntierPositif();
+        }
+
+
+        return new Partie(1,new ArrayList<Joueur>(), new ArrayList<Tour>());
+    }
 }
